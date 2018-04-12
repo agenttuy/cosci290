3dd9dc42a55413606e32a9263623bf8b7d865c7d
Sina <agenttuy@gmail.com>
2018-04-12 13:23:10 -0700

    Lab12

---
+++ group2/lab12/Lab12.java
@@ -0,0 +1,61 @@
+/*
+
+	<<!----- INSERT YOUR NAME HERE ------>>
+	Lab 12
+
+	Directions:
+	1. This application has bugs. Edit my code so that the program compiles and runs.
+	2. Comment every line of code.
+	3. Add code or modify the code so that the following output is achieved:
+
+	Sample output with odd number:
+
+	This program will check if a number is even or odd...
+	Please enter in a whole number...
+	3
+	The number 3 is odd...
+
+
+	Sample output with even number:
+
+	This program will check if a number is even or odd...
+	Please enter in a whole number...
+	2
+	The number 2 is even...
+
+	
+	Note:
+	Preserve the indentation of the code. 
+	Clean java code has:
+	 proper indentation
+	 variable declaration at the beginning of the program
+	 class name starts with a capital
+	 comments that explains the logic
+
+*/
+
+public class Lab2{
+
+	public static void main(String args){
+
+		Scanner input = new Scanner(system.in);
+		char num;
+
+		System.out.println(This program will check if a number"
+			+ " is even or odd...);
+
+		System.out.println("Please enter in a whole number...")
+
+		int num = input.next();
+
+		checkNum(num);
+
+	}
+
+	public void checkNums(double num){
+
+		//oops...  still need to implement
+
+		}
+	}
+}
