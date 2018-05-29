76c60ad05305852ff632e94c64d223febc092a75
agenttuy <agenttuy@gmail.com>
2018-05-29 13:36:12 -0400

    get subclass from superclass example

--- group4/TestGetClass.java
+++ group4/TestGetClass.java
@@ -11,17 +11,27 @@ public class TestGetClass{
   //main method
   public static void main(String[] args){
     
+    //declare list of supertype
     ArrayList<Armor> list = new ArrayList<Armor>();
     
+    //add different subclasses
     list.add(new Cloak());
     list.add(new Helmet());
     list.add(new Cloak());
     
+    //iterate through each object to do stuff with
     for(Armor a : list){
-      if(a.getClass().equals(Cloak.class))
+      //check if object is f a certain type to do certain things
+      if(a.getClass().equals(Cloak.class)){
         System.out.println("This is a cloak!");
-      else
+      }
+      else{
         System.out.println("This is another subclass of Armor!");
+        //below do specific methods of subclass below by casting
+        Helmet h = (Helmet)a;
+        h.setDurability(5); //this is a method of Helmet
+        System.out.println(h);
+      }
     }
     
   }//end main
